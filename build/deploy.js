1f56e9ea8eb5e26f3e61bdcbad75e7ee997a8a8f
Seyyed Hassan Asheghi <asheghi.sh@gmail.com>
2021-08-20 14:06:51 +0430

    minor changes

--- build/deploy.js
+++ build/deploy.js
@@ -2,12 +2,14 @@
 const process = require('process');
 const ghpages = require('gh-pages');
 
+console.log('Publishing to Github-Pages ...');
 ghpages.publish('dist', (err) => {
   if (err) {
-    console.error('Deploy Error!!!');
+    console.error('Deploy Failed');
+    console.error(err);
     process.exit(1);
   } else {
-    console.log('Deployed!!!');
+    console.log('Successfully Deployed to Github-Pages');
     process.exit(0);
   }
 });
